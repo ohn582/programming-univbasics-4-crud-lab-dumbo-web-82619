8827a9532885b93ab0f9d3e5102ad3b2d419479e
Neal JIn Oh <no4259@gmail.com>
2019-08-14 22:06:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -35,6 +35,6 @@ def retrieve_last_element_from_array(array)
   array[-1]
 end
 
-def update_element_from_index(array, 4, "totally")
-  array[4] = ["totally"]
+def update_element_from_index(array, index_number, element)
+  array[index_number] = element
 end
